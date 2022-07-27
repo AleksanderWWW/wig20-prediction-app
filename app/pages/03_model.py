88f87771e92e157b3e9cd14b9866ea298a376e30
AleksanderWWW <alwojnarowicz@gmail.com>
2022-07-27 16:11:37 +0200

    add mse and true-vs-predicted table display

--- app/pages/03_model.py
+++ app/pages/03_model.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
+
+from sklearn.metrics import mean_squared_error
 
 from src.model import train_lr
 
@@ -18,21 +21,27 @@ def run_page():
         return
 
     
-    model_type = st.selectbox("Select model type to train", ["Linear regression"])
+    model_type = st.selectbox("Select model type to use as regressor", ["Linear regression"])
 
     column_chosen = st.selectbox("Choose column to model", train_df.columns)
 
-    lags = st.slider("Number of lags in the model", min_value=1, max_value=len(train_df) // 2 + 1)
+    lags = st.slider("Number of lags in the model", min_value=1, max_value=len(train_df) // 2)
 
     model = {
         "Linear regression": train_lr
     }[model_type]
     
-    st.table(train_df.head())
+    
 
     res = model(train_df, "close", lags)
 
     predicted = res.predict(steps=len(test_df))
+
+    mse = mean_squared_error(test_df[column_chosen], predicted)
+
+    st.dataframe(pd.merge(test_df[column_chosen], predicted, left_index=True, right_index=True))
+
+    st.markdown(f"### Model MSE: {mse}")
 
     fig, ax = plt.subplots(figsize=(9, 4))
     train_df[column_chosen].plot(style="o", ax=ax, label='train')
