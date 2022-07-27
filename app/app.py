afb90aa2f8f99685945d962111524e7bdc7cdbd0
AleksanderWWW <alwojnarowicz@gmail.com>
2022-07-27 14:40:12 +0200

    clean main page

--- app/app.py
+++ app/app.py
@@ -3,12 +3,3 @@ import streamlit as st
 
 
 st.title("WIG-20 Predictive Modeling")
-
-
-# fig, ax = plt.subplots(figsize=(9, 4))
-# train_df[column_chosen].plot(style="o", ax=ax, label='train')
-# test_df[column_chosen].plot(style="o", ax=ax, label='test')
-# predicted.plot(style="o", ax=ax, label='predictions')
-# ax.legend()
-# st.pyplot(fig)
-
